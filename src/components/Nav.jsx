ffa36c8f5950ea731c8d17f35dc6d6427365170c
shariqsaleem27 <shariq.saleem06@gmail.com>
2024-01-30 07:10:58 +1100

    setup

---
+++ src/components/Nav.jsx
@@ -0,0 +1,20 @@
+import {headerLogo} from '../assets/images'f
+
+import {hamburger} from '../assets/icons'
+const Nav = () => {
+  return (
+    <header className='padding-x py-8 absolute z-10 w-full'>
+        <nav className='flex justify-between items-center max-center'>
+            <a href="/">
+                <img src={headerLogo} alt="logo" />
+            </a>
+            <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+
+
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Nav
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import Subscribe from './Subscribe';
+import Footer from './Footer';
+import CustomerReview from './CustomerReviews'
+
+export{
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    Footer,
+    CustomerReview,
+}
